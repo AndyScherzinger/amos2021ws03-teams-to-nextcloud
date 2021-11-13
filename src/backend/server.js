4f654eb3955520065a84c8ba5502c6000a0e26e9
Eric Schneider <eric.schneider96@gmail.com>
2021-11-13 14:24:59 +0100

    change login flow to oauth

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const fileUpload = require('express-fileupload');
-const { createClient } = require("webdav");
+const { createClient, AuthType } = require("webdav");
 const fetch = (...args) => import('node-fetch').then(({default: fetch})=>fetch(...args));
 const session = require('express-session')
 
@@ -10,6 +10,9 @@ const fs = require('fs');
 const app = express();
 const port = process.env.PORT
 const nextcloudUrl = process.env.NEXTCLOUD_URL
+const client_id = process.env.NEXTCLOUD_CLIENT
+const client_secret = process.env.NEXTCLOUD_SECRET
+
 
 
 
@@ -22,46 +25,47 @@ app.use(session({secret: 'keyboard cat', cookie: {}}));
 app.use(express.static('public'))
 
 app.get('/', async function(req, res){
+  console.log(req.session.id)
   res.sendFile('index.html')
 });
 app.get('/login', async function(req, res){
-  console.log(req.session.id)
-  let response = await fetch(`${nextcloudUrl}/index.php/login/v2`, {method:'POST', headers: {'User-Agent': 'Teams-To-Nextcloud'} })
-  let poll;
-  if(response.ok){
-    ({ poll, login } = await response.json())
-    console.log(JSON.stringify(poll))
-    req.session.token = poll.token;
-    req.session.endpoint = poll.endpoint;
-    res.redirect(login)
-  }
-  res.status(503).send('Could not fetch login')
+    console.log('session id /login: ' ,req.session.id);
+    res.redirect(`${nextcloudUrl}/index.php/apps/oauth2/authorize?client_id=${client_id}&state=${req.session.id}&response_type=code`)
+
 });
 app.get('/user', async function(req, res){
-  console.log(req.session.id)
-  console.log(req.session.token)
-  console.log(req.session.endpoint)
-  let requestBody = new URLSearchParams({
-    'token': req.session.token
-  });
-  let response = await fetch(req.session.endpoint, {method: 'POST', body:  requestBody}).catch(console.error)
-  if(response.ok){
-    let login = await response.json()
-    console.log(JSON.stringify(login))
-    req.session.username = login.loginName;
-    req.session.password = login.appPassword;
-    res.send()
-  }
-  else{
-    console.error(response.status)
-    res.status(response.status).send(response.statusText)
-  }
+    let code = req.query.code
+    let state = req.query.state
+    console.log('session id /user: ', req.session.id)
+    let response = await fetch(`${nextcloudUrl}/index.php/apps/oauth2/api/v1/token?grant_type=authorization_code&client_id=${client_id}&client_secret=${client_secret}&code=${code}&redirect_uri=${process.env.BASE_URL}`, {method: 'POST'}).catch(console.error)
+    if(response.ok){
+        let login = await response.json()
+        console.log(login)
+        req.session.access_token = login.access_token;
+        req.session.token_type = login.token_type;
+        req.session.expires_in = login.expires_in;
+        req.session.refresh_token = login.refresh_token;
+        req.session.user_id = login.user_id;
+        res.redirect('/')
+    }
+    else{
+        console.error(response.status)
+        res.status(response.status).send(response.statusText)
+    }
 });
 
 app.post('/upload', async function(req, res) {
+    console.log('/upload session id:', req.session.id)
+    console.log(req.session.access_token)
     const client = createClient(`${nextcloudUrl}/remote.php/webdav/`, {
-        username: req.session.username,
-        password: req.session.password
+        authType: AuthType.Token,
+        token : {
+            access_token: req.session.access_token,
+            token_type : req.session.token_type ,
+            expires_in : req.session.expires_in ,
+            refresh_token : req.session.refresh_token ,
+            user_id : req.session.user_id ,
+        }
     });
     console.log(req)
     console.log(req.files.sampleFile.name)
--- src/frontend/src/components/LoginButton.vue
+++ src/frontend/src/components/LoginButton.vue
@@ -24,21 +24,7 @@ export default {
   }, 
   methods:{
     initLogin(){
-        window.open(`${process.env.VUE_APP_BACKEND_BASE_URL}/login`, '_blank', 'popup,noopener')
-        this.intervalId = setInterval(() => { 
-            console.log('intervalId: ', this.intervalId)
-            console.log('loggedIn: ', this.loggedIn)
-            fetch(`${process.env.VUE_APP_BACKEND_BASE_URL}/user`, {credentials: 'same-origin'})
-                .then((response)=>{
-                    console.log('is response ok?', response.ok)
-                    if(response.ok){
-                        console.log("ok response")
-                        clearInterval(this.intervalId);
-                        this.loggedIn = true;
-                    }
-                })
-        }, 5000)
-
+        window.open(`${process.env.VUE_APP_BACKEND_BASE_URL}/login`, '_self')
     }
   }
 
